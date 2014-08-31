51e23bc821f5204ce93d26cbd87c6de36fa22bfc
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2014-09-01 00:01:28 +0400

    version 1

---
+++ src/me/truekenny/MyAntiPillar/MyAntiPillar.java
@@ -0,0 +1,64 @@
+package me.truekenny.MyAntiPillar;
+
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class MyAntiPillar extends JavaPlugin {
+    private Logger log = Logger.getLogger("Minecraft");
+    /**
+     * Экземпляр конфигурации
+     */
+    public FileConfiguration config;
+
+    public int minimumQuantityOf125;
+
+    public void onEnable() {
+        defaultConfig();
+
+        PluginManager pm = getServer().getPluginManager();
+        PlayerListener playerListener = new PlayerListener(this);
+        pm.registerEvents(playerListener, this);
+
+
+        log("MyLineagePvpSystem has been enabled!");
+    }
+
+    public void onDisable() {
+        log("MyLineagePvpSystem has been disabled.");
+    }
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public void log(String text, String color) {
+        this.log.info(color + text + ANSI_RESET);
+    }
+
+    public void log(String text) {
+        log(text, ANSI_GREEN);
+    }
+
+    public void defaultConfig() {
+        config = getConfig();
+
+        config.addDefault("minimumQuantityOf125", 30);
+        config.addDefault("reason", "You can not put this block here, try to strengthen the sides of the base unit");
+
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        minimumQuantityOf125 = config.getInt("minimumQuantityOf125");
+    }
+
+}
---
+++ src/me/truekenny/MyAntiPillar/PlayerListener.java
@@ -0,0 +1,85 @@
+package me.truekenny.MyAntiPillar;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PlayerListener implements Listener {
+    /**
+     * Экземпляр главного класса плагина
+     */
+    private final MyAntiPillar plugin;
+
+    private static final Set<String> spamBlocks = new HashSet<String>(Arrays.asList(
+            new String[]{"DIRT", "SAND", "COBBLESTONE"}
+    ));
+
+    public PlayerListener(MyAntiPillar instance) {
+        plugin = instance;
+        plugin.log("PlayerListener has been enabled!");
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onBlockPlaceEvent(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+        World world = player.getWorld();
+        Location location = block.getLocation();
+
+        Location testLocation;
+        Block testBlock;
+
+
+        // plugin.log(player.getName() + " place " + block);
+
+        if (!spamBlocks.contains(block.getType().toString())) {
+
+            return;
+        }
+
+        if (location.getBlockY() < 65) {
+
+            return;
+        }
+
+        int countBlock = 0;
+
+        int halfSize = 2;
+
+        for (int x = location.getBlockX() - halfSize; x <= location.getBlockX() + halfSize; x++) {
+            for (int z = location.getBlockZ() - halfSize; z <= location.getBlockZ() + halfSize; z++) {
+                for (int y = location.getBlockY() - 1 - halfSize * 2; y <= location.getBlockY() - 1; y++) {
+                    testLocation = new Location(world, x, y, z);
+                    testBlock = testLocation.getBlock();
+
+                    // plugin.log("" + testBlock);
+
+                    if (!testBlock.getType().toString().equals("AIR")) {
+                        countBlock++;
+
+                        if (countBlock >= plugin.minimumQuantityOf125) {
+
+                            return;
+                        }
+                    } // test !AIR
+                } // y
+            } // z
+        } // x
+
+        player.sendMessage(ChatColor.RED + plugin.config.getString("reason"));
+        event.setCancelled(true);
+
+        plugin.log(plugin.ANSI_RED + player.getName().toString() + " pillar blocked");
+    } // function
+}
+
